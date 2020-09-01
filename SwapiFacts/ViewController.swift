7b8f9bcdcd6fb37bbbffad86b45662669727d51f
Vladimir Spásov Dimitrov <spasov.vladimir@gmail.com>
2020-09-01 18:04:51 +0200

    Initial Commit

---
+++ SwapiFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwapiFacts
+//
+//  Created by Vladimir Spasov on 01/09/2020.
+//  Copyright © 2020 Vladimir Spasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
